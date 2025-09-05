d7ccdc7b002d4ef34ea93b7a5dee7e5a7076b087
Dmitry Kovalev <trigger1610@gmail.com>
2025-09-05 19:34:15 +0400

    Third commit with KMP initialization

---
+++ composeApp/src/androidUnitTest/kotlin/dmitrykovalev/stringlife/ComposeAppAndroidUnitTest.kt
@@ -0,0 +1,12 @@
+package dmitrykovalev.stringlife
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppAndroidUnitTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
