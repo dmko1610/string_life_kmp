6e03638dd6cb51db12dd6788cfa0e99bc37a3240
Dmitry Kovalev <trigger1610@gmail.com>
2025-09-05 18:23:29 +0400

    Second commit with initialization

---
+++ composeApp/src/commonTest/kotlin/dmitrykovalev/stringlife/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package dmitrykovalev.stringlife
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
