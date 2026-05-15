b645bd6b2d1ebd62f39934842479b7526fbbd902
Dmitry Kovalev <trigger1610@gmail.com>
2026-05-15 17:04:22 +0400

    Import local date

--- shared/src/commonMain/kotlin/dmitrykovalev/stringlife/viewmodel/InstrumentViewModel.kt
+++ shared/src/commonMain/kotlin/dmitrykovalev/stringlife/viewmodel/InstrumentViewModel.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.datetime.LocalDate
 
 sealed class UiState {
     object Loading : UiState()
